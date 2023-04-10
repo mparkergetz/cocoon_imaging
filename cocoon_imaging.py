9bf621d7b478ccef8dd2cf4c1b228519884bc0bd
mparkergetz <83979555+mparkergetz@users.noreply.github.com>
2023-04-10 13:40:18 -0700

    update

--- cocoon_imaging.py
+++ cocoon_imaging.py
@@ -3,6 +3,7 @@ import time
 import sys
 import os
 import re
+import json
 
 cam = Picamera2()
 cam_config = cam.create_still_configuration(main = {'size': (1920, 1080)}, lores = {'size': (640, 480)}, display = 'lores')
@@ -17,14 +18,18 @@ else:
 	last_im = im_out_list[-1]
 	im_num = int(re.search('\d+', im_out_list[-1])[0])
 	
-print(im_out_list)
-print(im_num)
+def dict_check():
+	home_files = os.listdir('./')
+	if 'label_table.json' in home_files:
+		with open('label_table.json', 'r') as file:
+			label_dict = json.load(file)
+	else:
+		label_dict = {}
+	return label_dict
+	
 
 
-def img_capture():
-	global im_num
-	
-	
+def img_capture(im_num, label_dict):
 	cam.start_preview(Preview.QTGL)
 	cam.start()
 	while True:
@@ -35,19 +40,41 @@ def img_capture():
 		cam.capture_file('./output/' + filename)
 		print(f'\nCaptured {filename}')
 		
-		input_choice = input('\n   Press "n" to take another image, press "q" to return to the main menu.\n\n')
+		label = input('Label: ')
+		
+		label_dict[filename] = label
+		
+		#print(f'\n\nImage: {filename}\nLabel: {label}\n\n')
+		
+		confirm = input('Confirm image/label info is good (y/n)  ')
+		if confirm == 'n':
+			label = input(f'\n\nImage: {filename}\nLabel: ')
+			label_dict[filename] = label		
+		
+		input_choice = input('\n   Press "n" to take another image, press "q" to save and return to the main menu.\n\n')
 		if input_choice == 'q':
+			with open('label_table.json', 'w') as file:
+				json.dump(label_dict, file)
+			
 			cam.stop_preview()
 			cam.stop()
 			break
 		else:
 			continue
 		
-		
+def write_out_json(label_dict):
+	import csv
+	fields = ['image', 'label']
+	with open('label_table.csv', 'w', newline = '') as file:
+		csvwriter = csv.writer(file)
+		csvwriter.writerow(fields)
+		for image in label_dict:
+			label = label_dict[image]
+			csvwriter.writerow([image,label])	
 
 def main():
 	while True:
-		print('\n\nWelcome to the Mason Bee Imaging Adventure.\n\nWould you like to:\n   1. Continue where you left off\n   2. Retake a particular image\n   3. Give up for now\n   4. There are no more cocoons to image\n\n')
+		print('\n\nWelcome to the Mason Bee Imaging Adventure.\n\nWould you like to:\n   1. Continue where you left off\n   2. Check your label dictionary\n   3. Exit\n\n   9. There are no more cocoons to image\n\n')
 
 		input_num = input('Enter your choice: ')
 
@@ -56,14 +83,26 @@ def main():
 				print('\n\nYour great journey begins.\n\n')
 			else:
 				print('\n\nWelcome back. We missed you.\n\n')
-			img_capture()
+				
+			label_dict = dict_check()
+			print(label_dict)
+			
+			img_capture(im_num, label_dict)
+			
 		elif input_num == '2':
-			print('\nThis feature has not been built yet. Shame my creator')
+			try:
+				for entry in label_dict:
+					print(f'{entry}: {label_dict[entry]}')
+			except:
+				print('\n\n*****Dict not in memory yet****\n\n')
 		elif input_num == '3':
-			print('\nThere is no escape')
+			print('\nGreat work today')
 			break
-		elif input_num == '4':
+		elif input_num == '9':
 			print('\nI am so proud of you. You are released from your burden.')
+			
+			write_out_json(label_dict)
+			
 			break
 		else:
 			print('\nTry again')
